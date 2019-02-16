1904371547b85975b4ba19f0b3324094733480c8
johan <pic16f630>
2019-02-16 13:39:58 +0100

    Add example

---
+++ examples/Discover.ino
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include "ESPDiscovery.h"
+
+char ssid[]     = "yourSSID";
+char password[] = "yourPASSWORD";
+
+WiFiUDP dsUDP;
+Discovery discovery(dsUDP, "HereIam");
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting ...");
+    delay(500);
+    }
+  Serial.println("Connected");  
+}
+
+void loop() {
+  discovery.handle();
+}
